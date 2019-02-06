ee3674bf3f7039aec244957816ec18b2d50430f0
jon <jontxust0@gmail.com>
2019-02-06 09:21:40 +0100

    crear los metodos sin implementar

---
+++ src/modelo/dao/UsuarioModelo.java
@@ -0,0 +1,21 @@
+package modelo.dao;
+
+import java.util.ArrayList;
+
+public class UsuarioModelo<Usuario>{
+public ArrayList<Usuario>selectTodos(){
+	//TODO devuelveun array listconla lista deusuariosde la BBDD
+
+}
+public void insertUsuario(Usuario usuario){
+	//TODO 
+}
+public void deleteUsuario(Usuario usuario){
+	//TODO 
+}
+
+public void updateUsuario(Usuario usuario){
+	//TODO 
+}
+
+}
