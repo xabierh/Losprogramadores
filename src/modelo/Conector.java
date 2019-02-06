4cbcce223dfccebed79849dc659b84862fdc3dea
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-02-06 09:16:07 +0100

    configuracion y conexion

---
+++ src/modelo/Conector.java
@@ -0,0 +1,38 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import modelo.Config;
+/**
+ * Clase Conector realiza y contiene la conexion con la BBDD
+ * @author Enaut Agirre
+ *
+ */
+public class Conector {
+	
+	protected Connection conexion;
+
+	Conector() {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				this.conexion = DriverManager.getConnection("jdbc:mysql://" + Config.HOST + "/" + Config.BBDD + "?zeroDateTimeBehavior=convertToNull", Config.USERNAME, Config.PASSWORD );
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+	
+}
---
+++ src/modelo/Config.java
@@ -0,0 +1,15 @@
+package modelo;
+/**
+ * Inteface Config creada almacenar guardar parametros
+ * de conexion con la BBDD
+ * 
+ * @author Enaut Agirre
+ *
+ */
+public interface Config {
+	//TODO rellenar el fichero de configuracion
+	public static final String BBDD = "";
+	public static final String HOST = "";
+	public static final String USERNAME = "";
+	public static final String PASSWORD = "";
+}
