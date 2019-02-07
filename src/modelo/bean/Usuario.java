a518552a588867dc65b59a66433dd80e3ba037c2
iker gomez <iker98g@gmail.com>
2019-02-07 12:37:01 +0100

    commit

---
+++ src/modelo/bean/Usuario.java
@@ -0,0 +1,59 @@
+package modelo.bean;
+
+import java.sql.Date;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String contraseña;
+	private String email;
+	private Date fecha_nac;
+	private Date fecha_reg;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getFecha_nac() {
+		return fecha_nac;
+	}
+	public void setFecha_nac(Date fecha_nac) {
+		this.fecha_nac = fecha_nac;
+	}
+	public Date getFecha_reg() {
+		return fecha_reg;
+	}
+	public void setFecha_reg(Date fecha_reg) {
+		this.fecha_reg = fecha_reg;
+	}
+	
+
+}
